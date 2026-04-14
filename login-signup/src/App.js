bac743f1da93806b412ceb0cda0de06c405e7dcd
SumaiyaMunas123 <2005sumaiyamunas@gmail.com>
2026-04-15 00:11:41 +0530

    changes made

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
+import LoginSignup from '../Components/LoginSignup/LoginSignup.jsx';
 
 function App() {
   return (
